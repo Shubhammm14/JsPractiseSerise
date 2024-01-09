bd30c30424efbb3a2b22f6bfbc52160026c67367
Shubhammm14 <101323348+Shubhammm14@users.noreply.github.com>
2024-01-09 20:10:19 +0000

    object commit

--- 02Basic/02Objects.js
+++ 02Basic/02Objects.js
@@ -51,3 +51,118 @@ juser.greetingTwo=function(){
 
 }
 console.log(juser.greetingTwo())//hellow world,shubham
+
+
+//==========================================================
+
+//const tinderUser=new Object()//singleton object
+
+const tinderUser={}
+ tinderUser.id='123'
+ tinderUser.name='shubham'
+ tinderUser.logIn=false
+ //console.log(tinderUser);//{ id: '123', name: 'shubham', logIn: false }
+/**
+ * 
+ */
+const regularUser={
+    email:'shubham@gmail.com',
+    fullDetails:{
+        userFullName:{
+            firstname:"shubham",
+            lastname:"shaw"
+        },
+        profession:{
+              IndividualProfession:'softwareDeveloper',
+              familyPorfession:'Bussiness'
+        },
+        studing:'btech'
+    }
+}
+console.log(regularUser)
+/**
+ * {
+  email: 'shubham@gmail.com',
+  fullDetails: {
+    userFullName: { firstname: 'shubham', lastname: 'shaw' },
+    profession: {
+      IndividualProfession: 'softwareDeveloper',
+      familyPorfession: 'Bussiness'
+    },
+    studing: 'btech'
+  }
+}
+ */
+console.log(regularUser.fullDetails.profession)
+/**
+ * {
+  IndividualProfession: 'softwareDeveloper',
+  familyPorfession: 'Bussiness'
+}
+ * 
+ */
+const obj1={1:'a',2:'b'}
+const obj2={3:'c',4:'d'}
+//const obj3={obj1,obj2}
+//const obj3=Object.assign(obj1,obj2)//{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }//no new object is creaed obj1 is just modified and assigned to obj 3
+//const obj3=Object.assign({},obj1,obj2)//{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }//none of the original objects are modified in this case ,a new object i screated and the values are assigned
+const obj3={...obj1,...obj2}//{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' } highly recomended to use spread method
+console.log(obj3)
+console.log(obj1)
+
+const users=[
+    {
+        id:1,
+        email:"shubahm@gmail.com"
+    },
+    {
+        id:2,
+        email:"shum@gmail.com"
+    },
+    {
+        id:3,
+        email:"hubahm@gmail.com"
+    },
+    {
+        id:4,
+        email:"ahm@gmail.com"
+    },
+]
+users[1].email
+console.log(tinderUser)
+
+console.log(Object.keys(tinderUser))//[ 'id', 'name', 'logIn' ]
+console.log(Object.values(tinderUser))//[ '123', 'shubham', false ]
+
+
+console.log(Object.entries(tinderUser))//[ [ 'id', '123' ], [ 'name', 'shubham' ], [ 'logIn', false ] ]  ecpair is converted in the form of arrays
+
+
+console.log(tinderUser.hasOwnProperty('logIn'))//true
+
+const course ={
+    courseName:"learning js",
+    price:"free",
+    courceInstructor:"hitesh"
+}
+//course.courceInstructor
+const{courceInstructor:inst}=course
+//console.log(courceInstructor)//hitesh
+console.log(inst);//hitesh
+
+//JSON
+// {
+//     "name":"hitesh",
+//     "coursename":"js in hindi",
+//     "price":"free",
+
+// }
+
+
+// [
+//     {},
+//     {},
+//     {},
+//     {}
+// ]
+
